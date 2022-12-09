ed9ccbac5ec89238c3f3dc55b9ffb00ca118c917
Artur <vanivskyi.artur@thinkful.com>
2022-12-09 16:48:06 -0500

    added nav bar and some functionality

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from "../../assests/crown.svg";
+import "./navigation.styles.scss";
+
+
+const Navigation = () => {
+  return (
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link classname="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;

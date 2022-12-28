9c9f50c25c4321f084a2831f2fe5f8dcba860c94
Artur <vanivskyi.artur@thinkful.com>
2022-12-28 14:28:55 -0500

    added checkput page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,20 +3,25 @@ import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 import { useContext } from "react";
 import { CartContext } from "../../context/cart.context";
-
+import { useNavigate } from "react-router-dom";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
 
-    const {cartItems} = useContext(CartContext)
-return (
+  const goToCheckoutHandler = () => {
+    navigate("/checkout")
+  }
+  return (
     <div className="cart-dropdown-container">
-        <div className="cart-items">
-            {cartItems.map(item => <CartItem key={item.id} cartItem={item}/>)}
-        </div>
-        <Button>GO TO CHECKOUT</Button>
-
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
-)
-}
+  );
+};
 
 export default CartDropdown;

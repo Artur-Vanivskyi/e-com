cccf74455bd90296c84929adb00a4a11725117a2
Artur <vanivskyi.artur@thinkful.com>
2023-01-01 18:01:47 -0500

    shop/any routes created

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 import "./shop.styles.scss";
 
 const Shop = () => {
@@ -7,7 +8,7 @@ const Shop = () => {
   return (
    <Routes>
     <Route index element={<CategoriesPreview />} />
-    <Route index element={<CategoriesPreview />} />
+    <Route path=":category" element={<Category />} />
    </Routes>
   )
 };

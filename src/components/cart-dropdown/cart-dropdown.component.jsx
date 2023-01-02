3eb31a0fa1c43fb5b61319f6587f97c4df607939
Artur <vanivskyi.artur@thinkful.com>
2023-01-02 18:27:32 -0500

    added final touch

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -15,9 +15,13 @@ const CartDropdown = () => {
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
+        {
+          cartItems.length ? (cartItems.map((item) => (
+            <CartItem key={item.id} cartItem={item} />
+          ))) : (
+            <span className="empty-message">Your cart is empty</span>
+          )
+        }
       </div>
       <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,12 +1,14 @@
 import "./directory-item.styles.scss";
+import { useNavigate } from "react-router-dom";
 
 const DirectoryItem = ({ category }) => {
+  const { title, imageUrl, route } = category;
+  const navigate = useNavigate();
 
-    const { title, imageUrl } = category
-
+  const onNavigateHandler = () => navigate(route);
 
   return (
-    <div className="directory-item-container">
+    <div className="directory-item-container" onClick={onNavigateHandler}>
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}

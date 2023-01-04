e60686fa9661812aa576a28e239b9a7bde434555
Artur <vanivskyi.artur@thinkful.com>
2023-01-04 13:44:17 -0500

    set store and added redux actions

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import "./index.scss";
 import App from "./App";
-import { UserProvider } from "./context/user.context";
+//import { UserProvider } from "./context/user.context";
 import { CategoriesProvider } from "./context/categories.context";
 import reportWebVitals from "./reportWebVitals";
 import { CartProvider } from "./context/cart.context";
+import { store } from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        {/* <UserProvider> */}
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        {/* </UserProvider> */}
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,12 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { rootReducer } from "./root-reducer";
 
 //root-reducer
+//to create a store we need root reducer that has combined reducers
+//middleware catch actions before they hit a reducer and log out the state 
+
+const middleWares = [logger];
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);

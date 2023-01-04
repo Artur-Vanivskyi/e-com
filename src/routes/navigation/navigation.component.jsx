06aca6efe1c5d584f4348052f93f5c78e092b6aa
Artur <vanivskyi.artur@thinkful.com>
2023-01-04 13:59:05 -0500

    added user redux syntax to navigation , built user and root-reducer

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,16 +1,22 @@
 import { Outlet, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { ReactComponent as CrwnLogo } from "../../assests/crown.svg";
 import { useContext } from "react";
-import { UserContext } from "../../context/user.context";
+//import { UserContext } from "../../context/user.context";
 import { CartContext } from "../../context/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import "./navigation.styles.scss";
+import { selectCurrentUser } from "../../store/user/user.secector";
+
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  //const { currentUser } = useContext(UserContext);
+  //useSelector -  gets the values for the whole app
+  const currentUser = useSelector(selectCurrentUser);
   const { isCartOpen } = useContext(CartContext);
+
   return (
     <>
       <div className="navigation">

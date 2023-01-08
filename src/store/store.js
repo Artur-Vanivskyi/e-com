dcc4d28938ede1e96da9eb7b5682e641d2157201
Artur <vanivskyi.artur@thinkful.com>
2023-01-07 20:27:52 -0500

    added reselect

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,26 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+//import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
+
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log("type", action.type);
+  console.log("payload", action.payload);
+  console.log("currentState", store.getState());
+
+  next(action);
+
+  console.log("next state: ", store.getState());
+};
 
 //root-reducer
 //to create a store we need root reducer that has combined reducers
-//middleware catch actions before they hit a reducer and log out the state 
+//middleware catch actions before they hit a reducer and log out the state
 
-const middleWares = [logger];
+const middleWares = [loggerMiddleware];
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 export const store = createStore(rootReducer, undefined, composedEnhancers);

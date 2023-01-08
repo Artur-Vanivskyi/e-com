b399bda00a48d783e6b9e0d7196d1a7cf416e704
Artur <vanivskyi.artur@thinkful.com>
2023-01-08 12:26:46 -0500

    finished redux layering

--- src/store/store.js
+++ src/store/store.js
@@ -1,41 +1,30 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import { persistStore, persistReducer } from "redux-persist"
-import storage from "redux-persist/lib/storage"
-//import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log("type", action.type);
-  console.log("payload", action.payload);
-  console.log("currentState", store.getState());
-
-  next(action);
-
-  console.log("next state: ", store.getState());
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
 };
 
-
-
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['user']
-}
-
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 
 //root-reducer
 //to create a store we need root reducer that has combined reducers
 //middleware catch actions before they hit a reducer and log out the state
 
-const middleWares = [loggerMiddleware];
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
+  Boolean
+);
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(persistedReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
